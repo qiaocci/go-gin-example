8f3728138d4d123de39f462ddef5a7c3909aab46
qiaocc <qiaocco@gmail.com>
2021-06-03 20:37:51 +0800

    增加 article接口定义

--- routers/api/v1/articles.go
+++ routers/api/v1/articles.go
@@ -1,24 +1,23 @@
 package v1
 
-//
-//import "github.com/gin-gonic/gin"
-//
-//func getArticle(c *gin.Context) {
-//
-//}
-//
-//func getArticles(c *gin.Context) {
-//
-//}
-//
-//func AddArticle(c *gin.Context) {
-//
-//}
-//
-//func EditArticle(c *gin.Context) {
-//
-//}
-//
-//func DeleteArticle(c *gin.Context) {
-//
-//}
+import "github.com/gin-gonic/gin"
+
+func GetArticle(c *gin.Context) {
+
+}
+
+func GetArticles(c *gin.Context) {
+
+}
+
+func AddArticle(c *gin.Context) {
+
+}
+
+func EditArticle(c *gin.Context) {
+
+}
+
+func DeleteArticle(c *gin.Context) {
+
+}
